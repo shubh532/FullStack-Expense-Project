f35ff8c20545ae97c61c6f891511c3d63ec8ad86
shubh532 <shubhammamahulkar2000>
2023-08-12 10:50:46 +0530

    Error Handling

--- Expense Frontend/Login.js
+++ Expense Frontend/Login.js
@@ -1,17 +1,23 @@
-const LoginBtn=document.getElementById("LogInBtn")
+const LoginBtn = document.getElementById("LogInBtn")
 
-LoginBtn.onclick= async(e)=>{
+LoginBtn.onclick = async (e) => {
     e.preventDefault()
-    const userdata={
-        email:document.forms["LoginForm"]["email"].value,
-        password:document.forms["LoginForm"]["password"].value,
+    const userdata = {
+        email: document.forms["LoginForm"]["email"].value,
+        password: document.forms["LoginForm"]["password"].value,
     }
     console.log(userdata)
-    try{
-        const Response= await axios.post("http://localhost:4000/expense-login",{...userdata})
-        console.log(Response,"Response")
-    }catch(err){
-        console.log(err,"Login Err")
+    try {
+        const Response = await axios.post("http://localhost:4000/expense-login", { ...userdata })
+        console.log(Response, "Response")
+        alert(Response.data.message)
+    } catch (err) {
+        if (err.response.status === 401) {
+            alert(err.response.data.message)
+        } else if (err.response.status === 500) {
+            alert("Server Error")
+        } else {
+            console.log(err)
+        }
     }
-
 }
